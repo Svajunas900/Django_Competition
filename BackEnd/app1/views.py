8df6aaee5de23aa94b25793c15428c21e1684354
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-31 11:48:12 +0200

    Create html for every view and GroupRequiredMixin

--- BackEnd/app1/views.py
+++ BackEnd/app1/views.py
@@ -1,27 +1,40 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseForbidden
 # Create your views here.
+
+
+class GroupRequiredMixin(LoginRequiredMixin):
+    group_required = None
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.group_required and not request.user.groups.filter(name=self.group_required).exists():
+            return HttpResponseForbidden("You do not have permission to view this page.")
+        return super().dispatch(request, *args, **kwargs)
+
 
 class HomePageView(TemplateView):
     template_name = "index.html"
 
 
-class CompetitionView(TemplateView):
-    pass
+class CompetitionView(GroupRequiredMixin, TemplateView):
+    template_name = "competition.html"
+    group_required = "Organizers"
+
 
 
 class RegistrationView(TemplateView):
-    pass
+    template_name = "registration.html"
 
 
 class Competitor_RegistrationsView(TemplateView):
-    pass
+    template_name = "competitor_registration.html"
 
 
 class BracketsView(TemplateView):
-    pass
+    template_name = "brackets.html"
 
 
 class ResultsView(TemplateView):
-    pass
+    template_name = "results.html"
