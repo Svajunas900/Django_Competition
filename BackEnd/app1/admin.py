1618e8d445275a054158c743dddf12f8c731cebb
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-02 14:47:12 +0200

    Add volume to docker-compose to save data between shut downs

--- BackEnd/app1/admin.py
+++ BackEnd/app1/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Competition, Competitor, Competitor_Level, City, Brackets, Weight, Age, Belt
+from .models import Competition, Competitor, CompetitorLevel, City, Brackets, Weight, Age, Belt
 # Register your models here.
 
 admin.site.register(Competition)
 admin.site.register(Competitor)
-admin.site.register(Competitor_Level)
+admin.site.register(CompetitorLevel)
 admin.site.register(City)
 admin.site.register(Brackets)
 admin.site.register(Weight)
